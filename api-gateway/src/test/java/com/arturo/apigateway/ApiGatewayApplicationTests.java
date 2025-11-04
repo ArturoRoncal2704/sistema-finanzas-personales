dcbe25a7765641d57823fb7c6697815f82bf55b3
Arturo Roncal <roncalhermenegildo@gmail.com>
2025-11-03 22:36:41 -0500

    chore: initial project setup with microservices structure

---
+++ api-gateway/src/test/java/com/arturo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
